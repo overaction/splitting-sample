da2cc30a9e50aadcde1d9e15960fa7854c245b43
overaction <kmc04@naver.com>
2020-04-06 14:13:23 +0000

    loadable component 사용법

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useState, Suspense } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-const SplitMe = React.lazy(() => import('./SplitMe'));
+import loadable from '@loadable/component';
+const SplitMe = loadable(() => import('./SplitMe'));
 
 function App() {
     const [visible, setVisible] = useState(false);
@@ -13,9 +14,7 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p onClick={onClick}>Hello</p>
-                <Suspense fallback={<div>loading...</div>}>
-                    {visible && <SplitMe />}
-                </Suspense>
+                {visible && <SplitMe />}
             </header>
         </div>
     )
