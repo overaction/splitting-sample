fcaa912593dc32a1e68a47501b98d3fd7615d0a8
overaction <kmc04@naver.com>
2020-04-06 14:23:57 +0000

    loadable component 컴포넌트 미리 불러오기

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import loadable from '@loadable/component';
-const SplitMe = loadable(() => import('./SplitMe'));
+const SplitMe = loadable(() => import('./SplitMe'), {
+    fallback: <div>loading...</div>
+});
 
 function App() {
     const [visible, setVisible] = useState(false);
     const onClick = () => {
         setVisible(true);
     };
+    const onMouseOver = () => {
+        SplitMe.preload();
+    }
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p onClick={onClick}>Hello</p>
+                <p onClick={onClick} onMouseOver={onMouseOver}>Hello</p>
                 {visible && <SplitMe />}
             </header>
         </div>
